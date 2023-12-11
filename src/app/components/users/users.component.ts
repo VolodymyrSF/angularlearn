671f282e02e24d92f2a6f080fb1ef9ca57cedcba
VSF <safjanik.volody@gmail.com>
2023-12-11 16:02:50 +0200

    base for angular project

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {IUser} from "../../interfaces/user.interface";
+import {NgForOf} from "@angular/common";
+import {UserComponent} from "../user/user.component";
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [
+    NgForOf,
+    UserComponent
+  ],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent implements OnInit{
+  users:IUser[]
+  constructor(private userService:UserService) {
+
+  }
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe(value => this.users=value)
+    }
+}
